baec98fe0886430c01890757e9545a0c1c1d8e1c
raiardia <ardiaanggreni@gmail.com>
2025-10-27 02:00:26 +0800

    Menambahkan file roomRoutes.js untuk mendefinisikan rute CRUD pada models Room

---
+++ backend/catalog_service/src/routes/roomRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+  getRooms,
+  getRoomById,
+  createRoom,
+  updateRoom,
+  deleteRoom,
+} from "../controllers/roomController.js";
+
+const router = express.Router();
+
+router.get("/", getRooms);
+router.get("/:id", getRoomById);
+router.post("/", createRoom);
+router.put("/:id", updateRoom);
+router.delete("/:id", deleteRoom);
+
+export default router;
