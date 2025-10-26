90420afd072eeb90827c7bec71e0b161a936fa0e
raiardia <ardiaanggreni@gmail.com>
2025-10-27 02:00:44 +0800

    Menambahkan file index.js untuk mengatur server Express dan koneksi MongoDB

---
+++ backend/catalog_service/src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import roomRoutes from "./routes/roomRoutes.js"; // sesuaikan path-nya
+
+dotenv.config();
+const app = express();
+
+// ✅ Izinkan akses dari frontend React
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+// routes
+app.use("/api/rooms", roomRoutes);
+
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`🏨 Catalog Service running on port ${PORT}`)
+    );
+    console.log("✅ MongoDB connected successfully");
+  })
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
